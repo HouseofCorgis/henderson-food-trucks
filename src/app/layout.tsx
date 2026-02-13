04c6372a55087cb2605951c3e627386b752c1be6
Jessica <jamminjessjams@gmail.com>
2026-02-13 09:45:28 -0500

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Fredoka, Source_Sans_3 } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const fredoka = Fredoka({
@@ -51,6 +52,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${fredoka.variable} ${sourceSans.variable}`}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0K2GGWHLY2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0K2GGWHLY2');
+          `}
+        </Script>
+      </head>
       <body className="font-body antialiased">{children}</body>
     </html>
   );
